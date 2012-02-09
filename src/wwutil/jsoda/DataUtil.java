c770bc0aa8493cb33a6bfaeb40bdcf3e811dd215
William Wong <williamw520@gmail.com>
2012-02-08 18:48:10 -0800

    Add license info.

--- src/wwutil/jsoda/DataUtil.java
+++ src/wwutil/jsoda/DataUtil.java
@@ -1,3 +1,18 @@
+/******************************************************************************
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.  If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Software distributed under the License is distributed on an "AS IS" basis, 
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for 
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is: Jsoda
+ * The Initial Developer of the Original Code is: William Wong (williamw520@gmail.com)
+ * Portions created by William Wong are Copyright (C) 2012 William Wong, All Rights Reserved.
+ *
+ ******************************************************************************/
+
 
 package wwutil.jsoda;
 
@@ -13,8 +28,6 @@ import wwutil.sys.TlsMap;
 
 class DataUtil
 {
-    public static final String  NULL_STR = "_NULL_";            // null encoding
-
     private static TlsMap.Factory<ObjectMapper> sTlsObjectMapper = new TlsMap.Factory<ObjectMapper>() {
         public ObjectMapper create(Object key) {
             return new ObjectMapper();
@@ -90,7 +103,7 @@ class DataUtil
         throws Exception
     {
         // Set null if input is null.
-        if (attrStr == null || attrStr.equals(DataUtil.NULL_STR))
+        if (attrStr == null)
             return null;
 
         if (valueType == String.class)
